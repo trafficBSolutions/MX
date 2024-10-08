cfe6cddf1d4dbcd9d9a90d1d0d100d83eca035dc
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-08 19:03:59 -0400

    Update services.jsx

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -7,18 +7,30 @@ const Service = () => {
             <Header/>
             <main className="service-main">
             <div className="material-image">
-          <img className="material-img" alt="Material WorX logo" src="../public/MX Photos/material worx.png" />
+            <img className="material-img" alt="Material WorX logo" src="../public/MX Logos/Material WorX.svg" />
         </div>
         <div className="material-service-container">
           <div className="service">
-          <h1 className="service-page-heading">Services</h1>
+          <h1 className="service-page-heading">SERVICES</h1>
           </div>
           </div>
-          <div className="material-services-buttons">
+          <div className="service-note-container">
+            <div className="service-note">
+  <h1 className="service-question">Looking for Highly Experienced Sign Services?</h1>
+  <p className="service-description">
+    At Material WorX, we have brought over 200 jobs of expertise in crafting high-quality, custom signage solutions. 
+    Whether you need eye-catching outdoor signs, impactful indoor displays, or specialized decals, our team ensures precision, durability,
+    and vibrant designs that reflect your brand's vision. Trust us to deliver on time, every time, with 
+    a commitment to excellence and customer satisfaction.
+  </p>
+  <p className="service-contact">Click any of the buttons below to learn more about our sign shop services.
+    Please contact us if you have any questions or concerns at <a href="tel:+17062630175">(706) 263-0175</a> or <a href="mailto:tbsolutions1999@gmail.com">tbsolutions1999@gmail.com</a>
+  </p>
+  <div className="material-services-buttons">
           <div className="signs-button">
             <a href="/custom-signs" className="custom-sign-service-button" type="button">
                     <img src="../public/service image buttons/sign.svg" alt="Custom Signage Icon" className="sign-icon" />
-                <span> CUSTOMIZABLE SIGNAGE</span>
+                <span className="sign-span-service"> CUSTOMIZABLE SIGNAGE</span>
             </a>
         </div>
             <div className="decals-stickers-button">
@@ -57,18 +69,6 @@ const Service = () => {
                 <span>VEHICLE FLEET & DECALED GRAPHICS</span></a>
             </div>
           </div>
-          <div className="service-note-container">
-            <div className="service-note">
-  <h1 className="service-question">Looking for Highly Experienced Sign Services?</h1>
-  <p className="service-description">
-    At Material WorX, we have brought over 200 jobs of expertise in crafting high-quality, custom signage solutions. 
-    Whether you need eye-catching outdoor signs, impactful indoor displays, or specialized decals, our team ensures precision, durability,
-    and vibrant designs that reflect your brand's vision. Trust us to deliver on time, every time, with 
-    a commitment to excellence and customer satisfaction.
-  </p>
-  <p className="service-contact">Click any of the buttons above to learn more about our sign shop services.
-    Please contact us if you have any questions or concerns at <a href="tel:+17062630175">(706) 263-0175</a> or <a href="mailto:tbsolutions1999@gmail.com">tbsolutions1999@gmail.com</a>
-  </p>
   </div>
   <section className="image-service-section">
     <div className="working-img-two">
@@ -77,13 +77,46 @@ const Service = () => {
   </section>
 </div>
 <section className="logo-design-section">
-    <h1 className="logo-new">New Logo?</h1>
-    <div className="new-logo-para">
-        <p className="new-logo-description"></p>
+<div className="new-logo-img-container">
+      <img alt="services" className="new-logo-img" src="../public/service image buttons/new logo symbol.svg" />
     </div>
+<div className="new-logo-para">
+<h1 className="logo-new">Need a New Logo?</h1>
+        <p className="new-logo-description">
+        Our team can 
+      create a unique and memorable logo that represents your brand's identity.
+      We offer a range of logo design services, including custom logo design, logo revisions, and logo branding.
+      Our team will work closely with you to understand your brand's values and goals, and create a logo that reflects your brand's personality and message.
+      We use the latest design tools and techniques to create a logo that is both visually appealing and effective in representing your brand.
+        </p>
+        <div className="new-logo-button-container">
     <a href="/new-logo" className="new-logo-button">New Logo</a>
+    </div>
+    </div>
 </section>
-
+<section className="web-design-section">
+      <div className="web-design-container">
+        <div className="web-design-text">
+          <h1 className="web-design-title">Need a New Website?</h1>
+          <p className="web-design-description">
+            Our web developer and graphic designer: William Rowell can create a custom website that is tailored to your business's needs and goals.
+            We offer a range of website design services, including website design, website development, and website maintenance.
+            Our team will work closely with you to understand your business's needs and goals, and create a website that is both visually appealing and effective in representing your business.
+            We use the latest design tools and techniques to create a website that is both visually appealing and effective in representing your business.
+            </p>
+            <p className="web-wix-tease">Tired of a simple bland Wix website with not many user friendly features?
+              We can upgrade your website from Wix to a hard coded and better CSS styled website that is more user friendly with coded Backend Programming and has more features than Wix.
+              If you have a Wix website and want to upgrade it to what websites were meant to be before Wix came out, contact us today!
+            </p>
+            </div>
+            <div className="web-design-button-container">
+              <a href="/new-website" className="web-design-button">Get a New Website</a>
+              </div>
+      </div>
+      <div className="web-design-img-container">
+        <img alt="services" className="web-design-img" src="../public/service image buttons/mern.svg" />
+      </div>
+      </section>
             </main>
             
         <footer className="material-footer">
@@ -100,7 +133,7 @@ const Service = () => {
             </div>
         <div className="site-material-footer__inner container container--narrow">
           <div className="footer-content">
-            <img className="mx-img" alt="TBS logo" src="../public/MX Photos/MX-removebg-preview.png" />
+          <img className="mx-img" alt="TBS logo" src="../public/MX Logos/MX.svg"/>
             <ul className="footer-navigate">
               <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
               <li><a className="footer-material-nav-link" href="/pay-invoice">Pay Invoice</a></li>
@@ -111,7 +144,7 @@ const Service = () => {
           <div className="footer-contact">
             <div className="statement-box">
               <p className="trademark-warning">
-                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src="../public/MX Photos/MX-removebg-preview.png" alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
+                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src="../public/MX Logos/MX.svg" alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
                 Unauthorized use of this logo is strictly prohibited and may result in legal action. 
                 All other trademarks, logos, and brands are the property of their respective owners.
               </p>
