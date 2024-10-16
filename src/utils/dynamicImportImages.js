7fc6da6674ed1e7871681567b330d232e371da4e
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 12:54:04 -0400

    Add files via upload

---
+++ src/utils/dynamicImportImages.js
@@ -0,0 +1,7 @@
+// src/utils/dynamicImportImages.js
+
+const images = import.meta.glob('../assets/**/*.{png,jpg,jpeg,svg,mp4,pdf}', { eager: true });
+
+console.log(images); // Log the keys to see the paths Vite generates
+
+export default images;
