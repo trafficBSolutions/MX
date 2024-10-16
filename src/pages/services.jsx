ae79a1942a186dabc88911f3f8fafa07c9869725
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 12:33:15 -0400

    Update services.jsx

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,13 +1,13 @@
 import '../css/services.css'
 import Header from '../components/headerviews/HeaderService'
-
+import images from '../utils/dynamicImportImages'
 const Service = () => {
     return (
         <div>
             <Header/>
             <main className="service-main">
             <div className="material-image">
-            <img className="material-img" alt="Material WorX logo" src="../public/MX Logos/Material WorX.svg" />
+            <img className="material-img" alt="Material WorX logo" src={images["../assets/MX Logos/Material WorX.svg"].default} />
         </div>
         <div className="material-service-container">
           <div className="service">
@@ -29,56 +29,56 @@ const Service = () => {
   <div className="material-services-buttons">
           <div className="signs-button">
             <a href="/custom-signs" className="custom-sign-service-button" type="button">
-                    <img src="../public/service image buttons/sign.svg" alt="Custom Signage Icon" className="sign-icon" />
+                    <img src={images["../assets/service image buttons/sign.svg"].default} alt="Custom Signage Icon" className="sign-icon" />
                 <span className="sign-span-service"> CUSTOMIZABLE SIGNAGE</span>
             </a>
         </div>
             <div className="decals-stickers-button">
                 <a href="/decals-stickers" className="decals-stickers-service-button">
-                <img src="../public/service image buttons/decal.svg" className="decal-icon"></img>
+                <img src={images["../assets/service image buttons/decal.svg"].default} className="decal-icon"></img>
                 <span>DECALS & STICKERS</span>
                 </a>
             </div>
             <div className="banner-button">
                 <a href="/banners" className="banner-service-button">
-                <img src="../public/service image buttons/banner.svg" className="banner-icon"></img>
+                <img src={images["../assets/service image buttons/banner.svg"].default} className="banner-icon"></img>
                 <span>BANNERS</span>
                 </a>
             </div>
             <div className="t-shirt-button">
                 <a href="/t-shirts-sweatshirts-jackets" className="t-shirt-service-button">
-                <img src="../public/service image buttons/t-shirt.svg" className="shirt-icon"></img>
+                <img src={images["../assets/service image buttons/t-shirt.svg"].default} className="shirt-icon"></img>
                 <span>T-SHIRTS SWEATSHIRTS JACKETS</span>
                 </a>
             </div>
             <div className="window-button">
                 <a href="/window-frost-tint" className="window-service-button">
-                <img src="../public/service image buttons/window.svg" className="window-icon"></img>
+                <img src={images["../assets/service image buttons/window.svg"].default} className="window-icon"></img>
                 <span>WINDOW FROSTING & TINTING</span>
                 </a>
             </div>
             <div className="drywall-button">
                 <a href="/drywall-floor-concrete" className="drywall-service-button">
-                <img src="../public/service image buttons/wall.svg" className="drywall-icon"></img>
+                <img src={images["../assets/service image buttons/wall.svg"].default} className="drywall-icon"></img>
                 <span>DRYWALL FLOOR & CONCRETE DECALS</span>
                 </a>
             </div>
             <div className="fleet-graphic-button">
                 <a href="/fleet-graphics" className="fleet-service-button">
-                <img src="../public/service image buttons/box truck.svg" className="fleet-icon"></img>
+                <img src={images["../assets/service image buttons/box truck.svg"].default} className="fleet-icon"></img>
                 <span>VEHICLE FLEET & DECALED GRAPHICS</span></a>
             </div>
           </div>
   </div>
   <section className="image-service-section">
     <div className="working-img-two">
-      <img alt="services" className="working-two" src="../public/MX Photos/calhoun.jpg" />
+      <img alt="services" className="working-two" src={images["../assets/MX Photos/calhoun.jpg"].default} />
     </div>
   </section>
 </div>
 <section className="logo-design-section">
 <div className="new-logo-img-container">
-      <img alt="services" className="new-logo-img" src="../public/service image buttons/new logo symbol.svg" />
+      <img alt="services" className="new-logo-img" src={images["../assets/service image buttons/new logo symbol.svg"].default} />
     </div>
 <div className="new-logo-para">
 <h1 className="logo-new">Need a New Logo?</h1>
@@ -114,26 +114,26 @@ const Service = () => {
               </div>
       </div>
       <div className="web-design-img-container">
-        <img alt="services" className="web-design-img" src="../public/service image buttons/mern.svg" />
+        <img alt="services" className="web-design-img" src={images["../assets/service image buttons/mern.svg"].default} />
       </div>
       </section>
             </main>
             
-        <footer className="material-footer">
+            <footer className="material-footer">
       <div className="social-icons">
                 <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-                    <img className="facebook-img" src="../public/social media/facebook.png" alt="Facebook" />
+                    <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
                 </a>
                 <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-                    <img className="tiktok-img" src="../public/social media/tiktok.png" alt="TikTok" />
+                    <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
                 </a>
                 <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-                    <img className="insta-img" src="../public/social media/instagram.png" alt="Instagram" />
+                    <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
                 </a>
             </div>
         <div className="site-material-footer__inner container container--narrow">
           <div className="footer-content">
-          <img className="mx-img" alt="TBS logo" src="../public/MX Logos/MX.svg"/>
+          <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default}/>
             <ul className="footer-navigate">
               <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
               <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
@@ -144,7 +144,7 @@ const Service = () => {
           <div className="footer-contact">
             <div className="statement-box">
               <p className="trademark-warning">
-                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src="../public/MX Logos/MX.svg" alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
+                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src={images["../assets/MX Logos/MX.svg"].default} alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
                 Unauthorized use of this logo is strictly prohibited and may result in legal action. 
                 All other trademarks, logos, and brands are the property of their respective owners.
               </p>
