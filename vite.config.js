9a9294b556ee29e92aa675e80cc9c2c6fcb71966
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 17:13:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'public'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public', 'index.html')
+    }
+  }
 })
