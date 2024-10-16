4ff429978bdfbd580df9b476e793f2f9e929e502
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 17:08:12 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  // Set the root directory to the project folder
   build: {
-    outDir: 'dist',  // Output directory for the built files
-    assetsDir: 'assets',  // Place assets inside an 'assets' directory in dist
-    emptyOutDir: true,  // Clear old files from dist before building
-    rollupOptions: {
-      input: './public/index.html',  // Entry point for the build
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',  // Create an alias for src folder to simplify imports
-    },
-  },
-});
+    outDir: 'dist'
+  }
+})
