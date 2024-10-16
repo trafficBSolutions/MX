84d53a4fa36b8e01328ba48c81e0d86e3417ac06
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 17:20:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, 'public', 'index.html')
+      input: 'index.html',
     }
   }
 })
