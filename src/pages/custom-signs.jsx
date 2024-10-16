07fea930aa30f709e64c4fb8366b50633bd69c56
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 12:27:39 -0400

    Update custom-signs.jsx

--- src/pages/custom-signs.jsx
+++ src/pages/custom-signs.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import '../css/signs.css';
 import '../css/headerfooter.css';
 import axios from 'axios';
-import MXSignGallery from '../components/photos/SignsGal';
+import MXSignGallery from '../photogallery/SignMXgallery';
 import Header from '../components/headerviews/HeaderSign';
+import images from '../utils/dynamicImportImages';
     const states = [
         { abbreviation: 'AL', name: 'Alabama' },
         { abbreviation: 'AK', name: 'Alaska' },
@@ -1134,20 +1135,20 @@ and what time you want an MX crew will arrive.</h1>
         </form>
       </main>
       <footer className="material-footer">
-      <div className="social-icons">
+                    <div className="social-icons">
                 <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-                    <img className="facebook-img" src="../public/social media/facebook.png" alt="Facebook" />
+                    <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
                 </a>
                 <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-                    <img className="tiktok-img" src="../public/social media/tiktok.png" alt="TikTok" />
+                    <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
                 </a>
                 <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-                    <img className="insta-img" src="../public/social media/instagram.png" alt="Instagram" />
+                    <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
                 </a>
             </div>
         <div className="site-material-footer__inner container container--narrow">
           <div className="footer-content">
-          <img className="mx-img" alt="TBS logo" src="../public/MX Logos/MX.svg"/>
+          <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default}/>
             <ul className="footer-navigate">
               <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
               <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
@@ -1158,7 +1159,7 @@ and what time you want an MX crew will arrive.</h1>
           <div className="footer-contact">
             <div className="statement-box">
               <p className="trademark-warning">
-                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src="../public/MX Logos/MX.svg" alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
+                <b className="warning-trade">WARNING:</b><b> Trademark Notice</b><img className="trademark-img" src={images["../assets/MX Logos/MX.svg"].default} alt="TBS Logo"></img> is a registered trademark of Traffic & Barrier Solutions, LLC. 
                 Unauthorized use of this logo is strictly prohibited and may result in legal action. 
                 All other trademarks, logos, and brands are the property of their respective owners.
               </p>
