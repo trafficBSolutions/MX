55d35ad6bfebfd015c39c37976198099ef72b088
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-16 17:47:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom'
 import { Home, Signs, Adhesive, Banner, Window, FleetGraphics, About, Invoice, Service, Logo, Contact, Decal, Shirt, Web, Blog, Error } from './pages';
 import axios from 'axios';
 import Navbar from './components/Navbar';
@@ -6,33 +6,32 @@ import { Toaster } from 'react-hot-toast';
 
 /* axios.defaults.baseURL = 'https://tbs-server.onrender.com'; */
 axios.defaults.baseURL = 'http://localhost:8000';
-axios.defaults.withCredentials = true;
-
+axios.defaults.withCredentials = true
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/custom-signs" element={<Signs />} />
-        <Route path="/drywall-floor-concrete" element={<Adhesive />} />
-        <Route path="/banners" element={<Banner />} />
-        <Route path="/window-frost-tint" element={<Window />} />
-        <Route path="/fleet-graphics" element={<FleetGraphics />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/pay-invoice" element={<Invoice />} />
-        <Route path="/services" element={<Service />} />
-        <Route path="/new-logo" element={<Logo />} />
-        <Route path="/contact-us" element={<Contact />} />
-        <Route path="/decals-stickers" element={<Decal />} />
-        <Route path="/t-shirts-sweatshirts-jackets" element={<Shirt />} />
-        <Route path="/new-website" element={<Web />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </Router>
-  );
+    <>
+     <Navbar />
+     <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="*" element={<Error/>}/>
+      <Route path="/custom-signs" element={<Signs/>}/>
+      <Route path="/drywall-floor-concrete" element={<Adhesive/>}/>
+      <Route path="/banners" element={<Banner/>}/>
+      <Route path="/window-frost-tint" element={<Window/>}/>
+      <Route path="/fleet-graphics" element={<FleetGraphics/>}/>
+      <Route path="/about-us" element={<About/>}/>
+      <Route path="/pay-invoice" element={<Invoice/>}/>
+      <Route path="/services" element={<Service/>}/>
+      <Route path="/new-logo" element={<Logo/>}/>
+      <Route path="/contact-us" element={<Contact/>}/>
+      <Route path="/decals-stickers" element={<Decal/>}/>
+      <Route path="/t-shirts-sweatshirts-jackets" element={<Shirt/>}/>
+      <Route path="/new-website" element={<Web/>}/>
+      <Route path="/blog" element={<Blog/>}/>
+     </Routes>
+    </>
+  )
 }
 
 export default App;
