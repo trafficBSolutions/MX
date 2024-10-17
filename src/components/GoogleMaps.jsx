cde4937fa0790f13e9c8bc2fa3ff308a4b8b1a2e
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 10:00:39 -0400

    Update GoogleMaps.jsx

--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -14,7 +14,7 @@ const MapInvoiceComponent = (props) => {
   const loadGoogleMapsScript = () => {
     if (!window.google || !window.google.maps) {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API}&libraries=geometry,places`;
       script.async = true;
       script.onload = () => {
         setGeocoder(new window.google.maps.Geocoder()); // Initialize Geocoder only once the script is loaded
