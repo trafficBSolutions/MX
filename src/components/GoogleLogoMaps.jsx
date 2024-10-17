4ac13f7e104a0ff4a5215b18ca11ad68fa14a59d
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 10:15:39 -0400

    Update GoogleLogoMaps.jsx

--- src/components/GoogleLogoMaps.jsx
+++ src/components/GoogleLogoMaps.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { GOOGLE_MAP_API } from '../constant/constant';
+import { REACT_APP_GOOGLE_MAPS_API_KEY} from '../constant/constant';
 
 const MapLogoComponent = (props) => {
   const [map, setMap] = useState(null);
@@ -14,7 +14,7 @@ const MapLogoComponent = (props) => {
   const loadGoogleMapsScript = () => {
     if (!window.google || !window.google.maps) {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API}&libraries=geometry,places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,places`;
       script.async = true;
       script.onload = () => {
         setGeocoder(new window.google.maps.Geocoder()); // Initialize Geocoder only once the script is loaded
