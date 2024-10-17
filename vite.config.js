2d3b8f3fd419bdfdccf0cecbec52a60f56880ea9
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 09:26:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',  // Change this to 'build' to match the Netlify setting
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html') // Point to the correct index.html
-    }
-  }
 })
