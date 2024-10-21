926548e69f7dec073090dc4ec66c9b9c3bdc142d
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-21 13:16:29 -0400

    Update pay-invoice.jsx

--- src/pages/pay-invoice.jsx
+++ src/pages/pay-invoice.jsx
@@ -160,6 +160,10 @@ const Invoice = () => {
     </div>
   </div>
 </footer>
+<div className="footer-copyright">
+      <p className-="footer-copy-p">&copy; 2024 Traffic & Barrier Solutions, LLC/Material WorX. 
+        This website was created by William Rowell. All rights reserved.</p>
+    </div>
         </div>
     )
 }
