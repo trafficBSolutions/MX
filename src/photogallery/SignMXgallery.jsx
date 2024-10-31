af86113c450bc16ecf31b1c2d496d52f75b5d008
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-31 14:54:52 -0400

    Update SignMXgallery.jsx

--- src/photogallery/SignMXgallery.jsx
+++ src/photogallery/SignMXgallery.jsx
@@ -3,6 +3,11 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import images from '../utils/dynamicImportImages';
 const photos = [
   {
+    src: images:['../assets/MX Photos/Sign Sample.svg']?.default || '',
+    width: 4032,
+    height: 3024
+  },
+  {
     src: images['../assets/MX Photos/church table.jpg']?.default || '',
     width: 4032,
     height: 3024
