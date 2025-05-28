229945fa0284a1d4efc275073d38e872c71d3824
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-28 14:17:56 -0400

    Update and rename decals-stickers.jsx to decals-stickers.js

--- src/pages/decals-stickers.js
+++ src/pages/decals-stickers.js
@@ -272,8 +272,8 @@ const formDataToSend = new FormData();
         <div className="material-type-container">
         <h1 className="sign-app-box">MATERIALS AVAILABLE FOR DECALS & STICKERS</h1>
   <div className="material-container">
-    <img className="orafol-img" src={images["../assets/MX Logos/Orafol-Logo.svg"].default}></img>
-    <img className="substance-img" src={images["../assets/MX Logos/substance-logo.svg"].default}></img>
+    <img className="orafol-img" src={images["Orafol-Logo.svg"]}></img>
+    <img className="substance-img" src={images["substance-logo.svg"]}></img>
 </div>
 </div>
         <form onSubmit={handleSubmit} className="decal-set -- box">
@@ -651,22 +651,23 @@ const formDataToSend = new FormData();
             </div>
             </form>
         </main>
-        <footer className="material-footer">
+      <footer className="material-footer">
   <div className="site-material-footer__inner">
-    <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default} />
+    <img className="mx-img" alt="TBS logo" src={images["MX Tan.svg"]} />
     <div className="footer-navigation-content">
       <h2 className="footer-title">Navigation</h2>
     <ul className="footer-navigate">
       <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
-      <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
+      <li><a className="footer-material-nav-link" href="/new-logo">New Logos</a></li>
       <li><a className="footer-material-nav-link" href="/services">Services</a></li>
+      <li><a className="footer-material-nav-link" href="/new-website">Websites</a></li>
       <li><a className="footer-material-nav-link" href="/contact-us">Contact Us</a></li>
     </ul>
     </div>
     <div className="footer-contact">
       <h2 className="footer-title">Contact</h2>
       <p className="contact-info">
-        <a className="will-phone" href="tel:+17062630175">Call: 706-263-0175</a>
+        <a className="will-phone" href="tel:+17062630175">Call: (706) 263-0175</a>
         <a className="will-email" href="mailto: tbsolutions1999@gmail.com">Email: tbsolutions1999@gmail.com</a>
         <a className="will-address" href="https://www.google.com/maps/place/Traffic+%26+Barrier+Solutions%2FMaterial+WorX+Sign+Shop/@34.5115302,-84.9476215,94m/data=!3m1!1e3!4m6!3m5!1s0x886007df83843f3b:0x84510d87790af625!8m2!3d34.5117917!4d-84.948025!16s%2Fg%2F11l28zhlzt?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D"
       >
@@ -677,13 +678,13 @@ const formDataToSend = new FormData();
     <div className="social-icons">
       <h2 className="footer-title">Follow Us</h2>
       <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-        <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
+        <img className="facebook-img" src={images["facebook.png"]} alt="Facebook" />
       </a>
       <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-        <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
+        <img className="tiktok-img" src={images["tiktok.png"]} alt="TikTok" />
       </a>
       <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-        <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
+        <img className="insta-img" src={images["instagram.png"]} alt="Instagram" />
       </a>
     </div>
   </div>
@@ -693,6 +694,6 @@ const formDataToSend = new FormData();
         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
     </div>
     </div>
-  )
-}
+    )
+  };
 export default Decal;
