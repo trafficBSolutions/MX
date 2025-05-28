ebd7ab05145d22116e9a248ac998b32b103221e8
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-28 14:18:28 -0400

    Update and rename drywall-floor-concrete.jsx to drywall-floor-concrete.js

--- src/pages/drywall-floor-concrete.js
+++ src/pages/drywall-floor-concrete.js
@@ -435,23 +435,23 @@ onChange={(e) => {
 <div className="placement-imgs">
 <div className="place-img-dry-container">
     <div className="place-img-container">
-      <img src={images["../assets/MX Photos/Nance 2.jpg"].default} alt="Placement=photo" className="place-img"/>
+      <img src={images["Nance 2.jpg"]} alt="Placement=photo" className="place-img"/>
         <h3 className="place-img-test">Drywall Graphics</h3>
 </div>
 <div className="place-img-container">
-        <img src={images["../assets/thick adhesive vinyls/floor graphics.jpg"].default} alt="Placement=photo" className="place-img"/>
+        <img src={images["floor graphics.jpg"]} alt="Placement=photo" className="place-img"/>
         <h3 className="place-img-test">Floor Graphics</h3>
 </div>
 <div className="place-img-container">
-        <img src={images["../assets/thick adhesive vinyls/concrete graphic.jpg"].default} alt="Placement=photo" className="place-img"/>
+        <img src={images["concrete graphic.jpg"]} alt="Placement=photo" className="place-img"/>
         <h3 className="place-img-test">Concrete Graphics</h3>
 </div>
 <div className="place-img-container">
-        <img src={images["../assets/thick adhesive vinyls/brick graphic.jpg"].default} alt="Placement=photo" className="place-img"/>
+        <img src={images["brick graphic.jpg"]} alt="Placement=photo" className="place-img"/>
         <h3 className="place-img-test">Brick Wall Graphics</h3>
 </div>
 <div className="place-img-container">
-        <img src={images["../assets/thick adhesive vinyls/cinder graphics.jpg"].default} alt="Placement=photo" className="place-img"/>
+        <img src={images["cinder graphics.jpg"]} alt="Placement=photo" className="place-img"/>
         <h3 className="place-img-test">Cinder Block Graphics</h3>
 </div>
     </div>
@@ -636,22 +636,23 @@ and what time you want an MX crew will arrive.</h1>
         </div>
         </form>
                     </main>
-                    <footer className="material-footer">
+      <footer className="material-footer">
   <div className="site-material-footer__inner">
-    <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default} />
+    <img className="mx-img" alt="TBS logo" src={images["MX Tan.svg"]} />
     <div className="footer-navigation-content">
       <h2 className="footer-title">Navigation</h2>
     <ul className="footer-navigate">
       <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
-      <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
+      <li><a className="footer-material-nav-link" href="/new-logo">New Logos</a></li>
       <li><a className="footer-material-nav-link" href="/services">Services</a></li>
+      <li><a className="footer-material-nav-link" href="/new-website">Websites</a></li>
       <li><a className="footer-material-nav-link" href="/contact-us">Contact Us</a></li>
     </ul>
     </div>
     <div className="footer-contact">
       <h2 className="footer-title">Contact</h2>
       <p className="contact-info">
-        <a className="will-phone" href="tel:+17062630175">Call: 706-263-0175</a>
+        <a className="will-phone" href="tel:+17062630175">Call: (706) 263-0175</a>
         <a className="will-email" href="mailto: tbsolutions1999@gmail.com">Email: tbsolutions1999@gmail.com</a>
         <a className="will-address" href="https://www.google.com/maps/place/Traffic+%26+Barrier+Solutions%2FMaterial+WorX+Sign+Shop/@34.5115302,-84.9476215,94m/data=!3m1!1e3!4m6!3m5!1s0x886007df83843f3b:0x84510d87790af625!8m2!3d34.5117917!4d-84.948025!16s%2Fg%2F11l28zhlzt?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D"
       >
@@ -662,13 +663,13 @@ and what time you want an MX crew will arrive.</h1>
     <div className="social-icons">
       <h2 className="footer-title">Follow Us</h2>
       <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-        <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
+        <img className="facebook-img" src={images["facebook.png"]} alt="Facebook" />
       </a>
       <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-        <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
+        <img className="tiktok-img" src={images["tiktok.png"]} alt="TikTok" />
       </a>
       <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-        <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
+        <img className="insta-img" src={images["instagram.png"]} alt="Instagram" />
       </a>
     </div>
   </div>
@@ -678,6 +679,6 @@ and what time you want an MX crew will arrive.</h1>
         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
     </div>
     </div>
-            );
-};
+    )
+  };
 export default Adhesive;
