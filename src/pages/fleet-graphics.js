39b8d5d18b960335c0aebd751b31713f249f5ccc
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-28 14:24:26 -0400

    Update and rename fleet-graphics.jsx to fleet-graphics.js

--- src/pages/fleet-graphics.js
+++ src/pages/fleet-graphics.js
@@ -316,7 +316,7 @@ const handleMakeChange = (e) => {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(freightlinerBoxTruckModels.filter((model) => !model.disabled)); // Show Freightliner box truck models
   } else {
-      setAvailableModels(freightlinerModels.filter((model) => !model.disabled)); // Show regular Freightliner models
+      setAvailableModels(freightlinerBoxTruckModels.filter((model) => !model.disabled)); // Show regular Freightliner models
   }
 } else if (selectedMake === 'GMC') {
   // Check if the vehicleType is "Box Truck"
@@ -330,7 +330,7 @@ const handleMakeChange = (e) => {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(hinoBoxTruckModels.filter((model) => !model.disabled)); // Show Hino box truck models
   } else {
-      setAvailableModels(hinoModels.filter((model) => !model.disabled)); // Show regular Hino models
+      setAvailableModels(hinoBoxTruckModels.filter((model) => !model.disabled)); // Show regular Hino models
   }
 }
 else if (selectedMake === 'International') {
@@ -338,7 +338,7 @@ else if (selectedMake === 'International') {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(internationalBoxTruckModels.filter((model) => !model.disabled)); // Show International box truck models
   } else {
-      setAvailableModels(internationalModels.filter((model) => !model.disabled)); // Show regular International models
+      setAvailableModels(internationalBoxTruckModels.filter((model) => !model.disabled)); // Show regular International models
   }
 } else if (selectedMake === 'Isuzu') {
   // Check if the vehicleType is "Box Truck"
@@ -352,14 +352,14 @@ else if (selectedMake === 'International') {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(kenworthBoxTruckModels.filter((model) => !model.disabled)); // Show Kenworth box truck models
   } else {
-      setAvailableModels(kenworthModels.filter((model) => !model.disabled)); // Show regular Kenworth models
+      setAvailableModels(kenworthBoxTruckModels.filter((model) => !model.disabled)); // Show regular Kenworth models
   }
 } else if (selectedMake === 'Mack') {
   // Check if the vehicleType is "Box Truck"
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(mackBoxTruckModels.filter((model) => !model.disabled)); // Show Mack box truck models
   } else {
-      setAvailableModels(mackModels.filter((model) => !model.disabled)); // Show regular Mack models
+      setAvailableModels(mackBoxTruckModels.filter((model) => !model.disabled)); // Show regular Mack models
   }
 } else if (selectedMake === 'Mercedes-Benz') {
   // Check if the vehicleType is "Box Truck"
@@ -373,7 +373,7 @@ else if (selectedMake === 'International') {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(mitsubishiFusoBoxTruckModels.filter((model) => !model.disabled)); // Show Mitsubishi Fuso box truck models
   } else {
-      setAvailableModels(mitsubishiFusoModels.filter((model) => !model.disabled)); // Show regular Mitsubishi Fuso models
+      setAvailableModels(mitsubishiFusoBoxTruckModels.filter((model) => !model.disabled)); // Show regular Mitsubishi Fuso models
   }
 } else if (selectedMake === 'Nissan') {
   // Check if the vehicleType is "Box Truck"
@@ -388,7 +388,7 @@ else if (selectedMake === 'International') {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(peterbiltBoxTruckModels.filter((model) => !model.disabled)); // Show Peterbilt box truck models
   } else {
-      setAvailableModels(peterbiltModels.filter((model) => !model.disabled)); // Show regular Peterbilt models
+      setAvailableModels(peterbiltBoxTruckModels.filter((model) => !model.disabled)); // Show regular Peterbilt models
   }
 } else if (selectedMake === 'Ram') {
   // Check if the vehicleType is "Box Truck"
@@ -402,7 +402,7 @@ else if (selectedMake === 'International') {
   if (selectedVehicleType === 'Box Truck') {
       setAvailableModels(scaniaBoxTruckModels.filter((model) => !model.disabled)); // Show Scania box truck models
   } else {
-      setAvailableModels(scaniaModels.filter((model) => !model.disabled)); // Show regular Scania models
+      setAvailableModels(scaniaBoxTruckModels.filter((model) => !model.disabled)); // Show regular Scania models
   }
 } else if (selectedMake === 'Volvo') {
   // Check if the vehicleType is "Box Truck"
@@ -840,22 +840,23 @@ onChange={(e) => {
 </div>
         </form>
         </main>
-        <footer className="material-footer">
+      <footer className="material-footer">
   <div className="site-material-footer__inner">
-    <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default} />
+    <img className="mx-img" alt="TBS logo" src={images["MX Tan.svg"]} />
     <div className="footer-navigation-content">
       <h2 className="footer-title">Navigation</h2>
     <ul className="footer-navigate">
       <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
-      <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
+      <li><a className="footer-material-nav-link" href="/new-logo">New Logos</a></li>
       <li><a className="footer-material-nav-link" href="/services">Services</a></li>
+      <li><a className="footer-material-nav-link" href="/new-website">Websites</a></li>
       <li><a className="footer-material-nav-link" href="/contact-us">Contact Us</a></li>
     </ul>
     </div>
     <div className="footer-contact">
       <h2 className="footer-title">Contact</h2>
       <p className="contact-info">
-        <a className="will-phone" href="tel:+17062630175">Call: 706-263-0175</a>
+        <a className="will-phone" href="tel:+17062630175">Call: (706) 263-0175</a>
         <a className="will-email" href="mailto: tbsolutions1999@gmail.com">Email: tbsolutions1999@gmail.com</a>
         <a className="will-address" href="https://www.google.com/maps/place/Traffic+%26+Barrier+Solutions%2FMaterial+WorX+Sign+Shop/@34.5115302,-84.9476215,94m/data=!3m1!1e3!4m6!3m5!1s0x886007df83843f3b:0x84510d87790af625!8m2!3d34.5117917!4d-84.948025!16s%2Fg%2F11l28zhlzt?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D"
       >
@@ -866,13 +867,13 @@ onChange={(e) => {
     <div className="social-icons">
       <h2 className="footer-title">Follow Us</h2>
       <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-        <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
+        <img className="facebook-img" src={images["facebook.png"]} alt="Facebook" />
       </a>
       <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-        <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
+        <img className="tiktok-img" src={images["tiktok.png"]} alt="TikTok" />
       </a>
       <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-        <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
+        <img className="insta-img" src={images["instagram.png"]} alt="Instagram" />
       </a>
     </div>
   </div>
@@ -882,7 +883,6 @@ onChange={(e) => {
         Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
     </div>
     </div>
-        
     )
-};
+  };
 export default FleetGraphics;
