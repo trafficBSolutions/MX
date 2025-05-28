1f910a616de3e135ad036c059d03d2ce450ff9c2
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-28 14:16:24 -0400

    Update and rename banners.jsx to banners.js

--- src/pages/banners.js
+++ src/pages/banners.js
@@ -468,7 +468,7 @@ onChange={(e) => {
     </div>
 <div className="place-banner-flex-container">
 <div className="place-banner-flex-container">
-        <img src={images["../assets/banner types/retract.png"].default} alt="Placement=photo" className="place-banner-img"/>
+        <img src={images["retract.png"]} alt="Placement=photo" className="place-banner-img"/>
         <h3 className="place-img-test">Retractable Stand</h3>
         <p className="retract-img-test">Retractable Stands are regular printed banners only they are not worked on by any of the banner types.
           Instead, they are placed on a retractable stand that can be pulled out and retracted when needed and are great for events and trade shows.
@@ -476,7 +476,7 @@ onChange={(e) => {
         </p>
   </div>
   <div className="place-banner-flex-containerr">
-        <img src={images["../assets/banner types/banner types.jpg"].default} alt="Placement=photo" className="place-banner-type-img"/>
+        <img src={images["banner types.jpg"]} alt="Placement=photo" className="place-banner-type-img"/>
         <h3 className="place-img-test">Banner Types</h3>
   </div>
     </div>
@@ -659,22 +659,23 @@ and what time you want an MX crew will arrive.</h1>
         </div>
         </form>
                     </main>
-                    <footer className="material-footer">
+      <footer className="material-footer">
   <div className="site-material-footer__inner">
-    <img className="mx-img" alt="TBS logo" src={images["../assets/MX Logos/MX.svg"].default} />
+    <img className="mx-img" alt="TBS logo" src={images["MX Tan.svg"]} />
     <div className="footer-navigation-content">
       <h2 className="footer-title">Navigation</h2>
     <ul className="footer-navigate">
       <li><a className="footer-material-nav-link" href="/about-us">About Us</a></li>
-      <li><a className="footer-material-nav-link" href="/blog">Blog</a></li>
+      <li><a className="footer-material-nav-link" href="/new-logo">New Logos</a></li>
       <li><a className="footer-material-nav-link" href="/services">Services</a></li>
+      <li><a className="footer-material-nav-link" href="/new-website">Websites</a></li>
       <li><a className="footer-material-nav-link" href="/contact-us">Contact Us</a></li>
     </ul>
     </div>
     <div className="footer-contact">
       <h2 className="footer-title">Contact</h2>
       <p className="contact-info">
-        <a className="will-phone" href="tel:+17062630175">Call: 706-263-0175</a>
+        <a className="will-phone" href="tel:+17062630175">Call: (706) 263-0175</a>
         <a className="will-email" href="mailto: tbsolutions1999@gmail.com">Email: tbsolutions1999@gmail.com</a>
         <a className="will-address" href="https://www.google.com/maps/place/Traffic+%26+Barrier+Solutions%2FMaterial+WorX+Sign+Shop/@34.5115302,-84.9476215,94m/data=!3m1!1e3!4m6!3m5!1s0x886007df83843f3b:0x84510d87790af625!8m2!3d34.5117917!4d-84.948025!16s%2Fg%2F11l28zhlzt?entry=ttu&g_ep=EgoyMDI0MDkyNC4wIKXMDSoASAFQAw%3D%3D"
       >
@@ -685,13 +686,13 @@ and what time you want an MX crew will arrive.</h1>
     <div className="social-icons">
       <h2 className="footer-title">Follow Us</h2>
       <a className="social-icon" href="https://www.facebook.com/tbssigns2022/" target="_blank" rel="noopener noreferrer">
-        <img className="facebook-img" src={images["../assets/social media/facebook.png"].default} alt="Facebook" />
+        <img className="facebook-img" src={images["facebook.png"]} alt="Facebook" />
       </a>
       <a className="social-icon" href="https://www.tiktok.com/@tbsmaterialworx?_t=8lf08Hc9T35&_r=1" target="_blank" rel="noopener noreferrer">
-        <img className="tiktok-img" src={images["../assets/social media/tiktok.png"].default} alt="TikTok" />
+        <img className="tiktok-img" src={images["tiktok.png"]} alt="TikTok" />
       </a>
       <a className="social-icon" href="https://www.instagram.com/tbsmaterialworx?igsh=YzV4b3doaTExcjN4&utm_source=qr" target="_blank" rel="noopener noreferrer">
-        <img className="insta-img" src={images["../assets/social media/instagram.png"].default} alt="Instagram" />
+        <img className="insta-img" src={images["instagram.png"]} alt="Instagram" />
       </a>
     </div>
   </div>
