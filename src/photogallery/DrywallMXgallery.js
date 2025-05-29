090ba6227dcc7643bc6379c08497a17d1309a1ee
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 09:11:50 -0400

    Update DrywallMXgallery.js

--- src/photogallery/DrywallMXgallery.js
+++ src/photogallery/DrywallMXgallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import images from '../utils/dynamicImportImages';
 const photos = [
@@ -34,7 +34,7 @@ export default function MXDrywallGallery() {
             setModalImage(photo.src);
             setIsModalOpen(true);
           }}>
-            <img src={photo.src} alt={`Photo ${index}`} />
+            <img src={photo.src} alt={`Wall result ${index + 1}`} />
           </div>
         ))}
       </div>
