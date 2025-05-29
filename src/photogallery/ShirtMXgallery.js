ba00cf68155ec380b7eae0cdb20bccfc52cfa95b
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 09:12:47 -0400

    Update ShirtMXgallery.js

--- src/photogallery/ShirtMXgallery.js
+++ src/photogallery/ShirtMXgallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import images from '../utils/dynamicImportImages';
 const photos = [
@@ -49,7 +49,7 @@ export default function MXShirtGallery() {
             setModalImage(photo.src);
             setIsModalOpen(true);
           }}>
-            <img src={photo.src} alt={`Photo ${index}`} />
+            <img src={photo.src} alt={`Shirt result ${index + 1}`} />
           </div>
         ))}
       </div>
