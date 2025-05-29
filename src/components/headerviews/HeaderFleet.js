106462e19a34018ec5f0046e69db8860d9a375b3
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 09:38:38 -0400

    Update HeaderFleet.js

--- src/components/headerviews/HeaderFleet.js
+++ src/components/headerviews/HeaderFleet.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import { useState } from 'react'; 
 import images  from '../../utils/dynamicImportImages';
 function Header() {
     const [isNavOpen, setIsNavOpen] = useState(false);
@@ -22,7 +22,7 @@ function Header() {
                 <ul className="main-material-nav-list">
                     <li><a className="main-material-nav-link" href="/new-logo">New Logos</a></li>
                     <li>
-                        <a className="main-material-nav-link">Sign Shop Services</a>
+                        <p className="main-material-nav-link">Sign Shop Services</p>
                         <ul className="sub-material-nav-list">
                             <li><a className="main-material-nav-link" href="/custom-signs">Custom Signs</a></li>
                             <li><a className="main-material-nav-link" href="/decals-stickers">Decals & Stickers</a></li>
@@ -30,7 +30,7 @@ function Header() {
                             <li><a className="main-material-nav-link" href="/t-shirts-sweatshirts-jackets">Custom Apparel</a></li>
                             <li><a className="main-material-nav-link" href="/window-frost-tint">Window Frosting & Tinting</a></li>
                             <li><a className="main-material-nav-link" href="/drywall-floor-concrete">Drywall, Floor & Concrete Vinyl</a></li>
-                            <li><a className="main-material-nav-link-view" href="">Fleet Graphics</a></li>
+                            <li><a className="main-material-nav-link-view" href="/fleet-graphics">Fleet Graphics</a></li>
                         </ul>
                     </li>
                     <li><a className="main-material-nav-link" href="/new-website">Websites</a></li>
