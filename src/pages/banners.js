8dcecf74ca53010173012241e6837191ac283a43
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 19:35:41 -0400

    Update banners.js

--- src/pages/banners.js
+++ src/pages/banners.js
@@ -606,7 +606,7 @@ and what time you want an MX crew will arrive.</h1>
   />
 <p className="terms-text">
   <strong>PLEASE READ AND CHECK:</strong><br />
-  You agree to pay for all custom signs and labor once production begins. No cancellations after materials are ordered or work has started.
+  You agree to pay for all custom banners and labor once production begins. No cancellations after materials are ordered or work has started.
 </p>
 </div>
 {errors.terms && <div className="error-message">{errors.terms}</div>}
