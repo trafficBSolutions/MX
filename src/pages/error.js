cc34d21cb6baa0760b89a337566172ffd851d0ba
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-14 12:21:23 -0400

    Update error.js

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,3 +1,4 @@
+import FooterCopyright from '../components/FooterCopyright';
 import '../css/error.css';
 import '../css/headerfooter.css';
 import Header from '../components/headerviews/HeaderError';
@@ -70,10 +71,7 @@ const Error = () => {
     </div>
   </div>
 </footer>
-<div className="footer-copyright">
-      <p className="footer-copy-p">&copy; 2026 Traffic & Barrier Solutions, LLC/Material WorX - 
-        Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
-    </div>
+<FooterCopyright />
     </div>
     )
   };
