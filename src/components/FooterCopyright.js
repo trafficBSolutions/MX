b9346a8dcf94cd972806e8b008db2bb49f27a007
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-14 12:17:55 -0400

    Create FooterCopyright.js

---
+++ src/components/FooterCopyright.js
@@ -0,0 +1,8 @@
+const FooterCopyright = () => (
+  <div className="footer-copyright">
+    <p className="footer-copy-p">&copy; 2026 Traffic & Barrier Solutions, LLC/Material WorX - 
+      Website MERN Stack Coded & Deployed by <a className="footer-face" href="/portfolio">MX Systems</a> - All Rights Reserved.</p>
+  </div>
+);
+
+export default FooterCopyright;
