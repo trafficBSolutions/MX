7f7ec16567ffe158b3390fe0e7d816d862af5542
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-14 12:20:29 -0400

    Update decals-stickers.js

--- src/pages/decals-stickers.js
+++ src/pages/decals-stickers.js
@@ -1,3 +1,4 @@
+import FooterCopyright from '../components/FooterCopyright';
 import {useState} from 'react'
 import '../css/headerfooter.css';
 import '../css/decal.css';
@@ -684,10 +685,7 @@ const formDataToSend = new FormData();
     </div>
   </div>
 </footer>
-<div className="footer-copyright">
-      <p className="footer-copy-p">&copy; 2026 Traffic & Barrier Solutions, LLC/Material WorX - 
-        Website MERN Stack Coded & Deployed by <a className="footer-face"href="https://www.facebook.com/will.rowell.779" target="_blank" rel="noopener noreferrer">William Rowell</a> - All Rights Reserved.</p>
-    </div>
+<FooterCopyright />
     </div>
     )
   };
